810996a67eff5270a42e483bdb49f57b2dd3c134
Dave Calderwood <davidhcalderwood@gmail.com>
2019-09-19 15:13:05 -0400

    Routing set up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Favorites from './pages/Favorites';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Lets make a new Profile!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+class App extends Component {
+ 
+  render() {
+    return (
+    <div class="full-body">
+      <Router>
+      <div className="App">  
+        <div className="header">
+        <div className="header-links">
+          <Link to="/">
+              <div className="home">
+                  <h3>Home</h3>
+              </div>
+            </Link>
+
+            <Link to="/favorites">
+              <div className="favorites">
+                <h3>Favorites</h3>
+              </div>
+            </Link>
+
+            <Link to="/about">
+              <div className="about">
+                  <h3>About</h3>
+              </div>
+            </Link>
+          </div>
+        </div>
+          
+        </div>
+
+        <div className="main">
+          {/* Route paths */}
+          <Route exact path="/" component={Home} />
+          <Route path="/favorites" component={Favorites} />
+          <Route path="/about" component={About} />
+        
+        </div>
+      </Router>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
---
+++ src/pages/About.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+
+
+export default class About extends Component {
+    render() {
+        return (
+            <div className="about">
+                <p>Top</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>About</p>
+                <p>Bottom</p>
+            </div>
+        );
+    }
+}
