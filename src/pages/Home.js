ea4c8521d167d719da976bd63be9ed6476f66cdc
Dave Calderwood <davidhcalderwood@gmail.com>
2019-09-26 11:33:21 -0400

    changed flexbox to masonry

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,40 +3,81 @@ import CardData from '../data/data.json';
 
 export default class Home extends Component {
 
-    constructor ()
+    constructor (props)
     {
-      super();
+      super(props);
       this.state={
         data: [],
         id: Number,
         name: '',
-        agency: ''
+        agency: '',
         
+        cardOption: true
       }
       console.log('Data', CardData);
+    }
 
+    changeCard = () => {
+        this.setState({
+            cardOption: !this.state.cardOption
+        });
+        console.log('New Card:')
     }
 
     render() {
         return (
             <div className="homepage">
 
-                {CardData.map((cardDetail, index) => {
-                    return <div>
+                {/* {CardData.slice(0, 4).map((cardDetail, index) => {
+                    return <div key={index}>
 
                         <div className="card">
                             <div className="card-header"> {cardDetail.name}
-                                <i className="fa fa-ellipsis-v fa-lg"></i>
+                                <i className="fa fa-ellipsis-v fa-lg" onClick={this.changeCard}></i>
                             </div>
+
                             <div className="card-content">
-                                {cardDetail.content}
+                                    <strong>{cardDetail.subTitle}</strong><br/>
+                                    {cardDetail.content}
+
+                                    <div className="dropdown">
+                                        {!this.state.cardOption && <Dropdown />}
+                                    </div>
                             </div>
                         </div>
-                        
+
                     </div>
-                })}
+                })} */}
+
+                <div className="masonry">
+                    <div className="item">Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top Top </div>
+                    <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+
+                    <div className="item">
+                        <div className="profile">
+                            <img src="../images/profile.jpg" alt="Profile"></img>
+                        </div>
+                    </div>
+
+                    <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <div className="item">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div>
+                    <div className="item">Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom Bottom </div>
+                </div>
             </div>
         );
         
     }
 }
+// const Dropdown = () => (
+//     <div className="other-cards">
+//         {CardData.map((cardDetail, index) => {
+//             return <div key={index}>
+//                 <div className="new-cards">
+//                     {cardDetail.name}
+//                 </div>
+//             </div>
+//         })}
+//     </div>
+// )
