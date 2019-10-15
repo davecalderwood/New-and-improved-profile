653850f6595b210db8ba59e0f239e5a5bd3df67f
Dave Calderwood <davidhcalderwood@gmail.com>
2019-10-15 15:57:04 -0400

    added favorites page base layout

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -5,7 +5,41 @@ export default class Favorites extends Component {
     render() {
         return (
             <div className="favorites">
-                Favorites
+                <div className="row">
+                    <div className="card">1</div>
+                    <div className="card">2</div>
+                    <div className="card">3</div>
+                    <div className="card">4</div>
+                    <div className="card">5</div>
+                    <div className="card">6</div>
+                    <div className="card">7</div>
+                    <div className="card">8</div>
+                    <div className="card">9</div>
+                </div>
+
+                <div className="row">
+                <div className="card">1</div>
+                    <div className="card">2</div>
+                    <div className="card">3</div>
+                    <div className="card">4</div>
+                    <div className="card">5</div>
+                    <div className="card">6</div>
+                    <div className="card">7</div>
+                    <div className="card">8</div>
+                    <div className="card">9</div>
+                </div>
+
+                <div className="row">
+                <div className="card">1</div>
+                    <div className="card">2</div>
+                    <div className="card">3</div>
+                    <div className="card">4</div>
+                    <div className="card">5</div>
+                    <div className="card">6</div>
+                    <div className="card">7</div>
+                    <div className="card">8</div>
+                    <div className="card">9</div>
+                </div>
             </div>
         );
     }
